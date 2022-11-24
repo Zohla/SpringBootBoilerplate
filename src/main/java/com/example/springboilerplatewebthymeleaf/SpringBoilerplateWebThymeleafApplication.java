be6642a93cbb3f7fd54a17117585a9ab27841d42
Maren Lilleberre <marenlilleberre@gmail.com>
2022-11-24 16:51:48 +0100

    Initial commit

---
+++ src/main/java/com/example/springboilerplatewebthymeleaf/SpringBoilerplateWebThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboilerplatewebthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoilerplateWebThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoilerplateWebThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboilerplatewebthymeleaf/SpringBoilerplateWebThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboilerplatewebthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerplateWebThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
